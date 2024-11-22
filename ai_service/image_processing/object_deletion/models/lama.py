4ca731c4069640dba857b8df2e25ffab81f168b5
PawelD16 <pawel200216@interia.eu>
2024-11-22 11:10:22 +0100

    Remove unnecessary channels to make AI Microservice work with 4 channel images

--- ai_service/image_processing/object_deletion/models/lama.py
+++ ai_service/image_processing/object_deletion/models/lama.py
@@ -8,7 +8,9 @@ class LamaInpaintingModel(ObjectDeletionModel):
         self.model = SimpleLama(device="cpu")
 
     def inpaint(self, image: Image.Image, mask: Image.Image) -> Image.Image:
-        resized_mask = mask.resize(image.size, Image.LANCZOS)
+        converted_image = image.convert("RGB")
+
+        resized_mask = mask.resize(converted_image.size, Image.LANCZOS)
         resized_mask = resized_mask.convert("L")
         resized_binary_mask = resized_mask.point(lambda p: 255 if p > 128 else 0)
-        return self.model(image, resized_binary_mask)
+        return self.model(converted_image, resized_binary_mask)
